0e30016dfdf3e31e677f6741c7fdd10cb8818986
Muhammed-Abdulrhman <mohammedabdowork@gmail.com>
2025-08-05 20:33:22 +0300

    Update settings.py

--- ti_web/ti_web/settings.py
+++ ti_web/ti_web/settings.py
@@ -27,18 +27,21 @@ SECRET_KEY = 'django-insecure-yxr)%h(h(-+4*#27n72xyo(&s$ia%_!kfd81ph13inqbegd^p0
 DEBUG = False
 #ALLOWED_HOSTS = ['']
 
+
+
 # ALLOWED_HOSTS = [
-#     os.environ.get('https://ti-team-website-production.up.railway.app/', '*'),
+#     os.environ.get('RAILWAY_STATIC_URL', 'ti-team-website-production.up.railway.app'),
 #     'localhost',
 #     '127.0.0.1'
 # ]
-# CSRF_TRUSTED_ORIGINS = ['https://ti-team-website-production.up.railway.app']
 
-ALLOWED_HOSTS = [
-    os.environ.get('RAILWAY_STATIC_URL', 'ti-team-website-production.up.railway.app'),
-    'localhost',
-    '127.0.0.1'
-]
+# أضف هذه الإعدادات في ملف settings.py
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+
+DEBUG = False 
+ALLOWED_HOSTS = ['*'] 
 
 CSRF_TRUSTED_ORIGINS = [
     'https://ti-team-website-production.up.railway.app'
@@ -148,7 +151,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Destination for `collectstatic`
 
-MEDIA_URL = '/media/' 
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
@@ -165,6 +168,7 @@ EMAIL_HOST_USER = 'muhamedabdodeveloper@gmail.com'  # Your email address
 EMAIL_HOST_PASSWORD = 'pmso dwly aflk pdmc'  # Your email password or app-specific password
 DEFAULT_FROM_EMAIL = 'muhamedabdodeveloper@gmail.com'  # Default sender email
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 
