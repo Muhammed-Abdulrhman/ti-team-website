ddfa911220e0d893a1d2e107e7df1a7025e06e0c
Muhammed-Abdulrhman <mohammedabdowork@gmail.com>
2025-08-05 19:56:14 +0300

    Update settings.py

--- ti_web/ti_web/settings.py
+++ ti_web/ti_web/settings.py
@@ -83,6 +83,7 @@ WSGI_APPLICATION = 'ti_web.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 import dj_database_url
+DATABASE_URL = postgres://username:password@host:5432/database_name
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -153,6 +154,7 @@ EMAIL_HOST_USER = 'muhamedabdodeveloper@gmail.com'  # Your email address
 EMAIL_HOST_PASSWORD = 'pmso dwly aflk pdmc'  # Your email password or app-specific password
 DEFAULT_FROM_EMAIL = 'muhamedabdodeveloper@gmail.com'  # Default sender email
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 
